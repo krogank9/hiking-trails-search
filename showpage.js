0f712cc44087d3bce10f39603bdc583f0a39e0d8
krogank9 <wifi.mouse.app@gmail.com>
2019-08-06 21:29:46 -0400

    fix scrolling on mobile

--- showpage.js
+++ showpage.js
@@ -1,26 +1,53 @@
+let searchScroll = 0;
+let showingScrollPage = false;
+
+function isMobile() {
+	return !isDesktop();
+}
+
 function isDesktop() {
 	return $(window).width() >= 1025;
 }
 
 function showAppInfoPage() {
+	if(showingScrollPage) {
+		searchScroll = window.scrollY;
+		showingScrollPage = false;
+	}
+	
 	$(".trail_info").addClass("d-none");
 	$(".app_info").removeClass("d-none");
 	
 	$("#search_view_container").removeClass("active_view");
 	$("#trail_view_container").addClass("active_view");
+	
+	if(isMobile())
+		window.scrollTo(0,0);
 }
 
 function showTrailPage() {
+	if(showingScrollPage) {
+		searchScroll = window.scrollY;
+		showingScrollPage = false;
+	}
+	
 	$(".trail_info").removeClass("d-none");
 	$(".app_info").addClass("d-none");
 	
 	$("#search_view_container").removeClass("active_view");
 	$("#trail_view_container").addClass("active_view");
+	
+	if(isMobile())
+		window.scrollTo(0,0);
 }
 
-function showSearchPage() {
+function showSearchPage() {	
 	$("#search_view_container").addClass("active_view");
 	$("#trail_view_container").removeClass("active_view");
+
+	showingScrollPage = true;
+	if(isMobile())
+		window.scrollTo(0,searchScroll);
 }
 
 $(".back_to_search_button").click(function(){
